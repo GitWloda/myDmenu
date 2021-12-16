4b77c4aae0b92ab5b7114043ebf0ea8d5cdc1a52
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-12-16 14:48:19 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+mkdir -p ~/git
+cd ~/git
+
+sudo pacman -S base-devel curl gzip jq skim
+git clone https://github.com/x7429/shaur
+cd shaur
+sudo make install
+cd ..
+
+git clone https://github.com/noctuid/zscroll
+cd zscroll
+sudo python3 setup.py install
+cd ..
+
+sudo pacman -S terminator
+terminator -e "cd ~/git && git clone https://github.com/sebastiencs/icons-in-terminal.git && cd icons-in-terminal.git && ./install.sh && cd .."
+sudo pacman -Rcns terminator
+
+sudo pacman -S clang gperf rsync wget
+git clone https://github.com/sebastiencs/ls-icons.git
+cd ls-icons
+./bootstrap
+export CFLAGS=-DNO_TRUE_COLOURS=1 
+export CC=clang CXX=clang++
+./configure --prefix=/opt/coreutils
+make
+sudo make install
+
+
+git clone https://github.com/GitWloda/myi3Config.git
+cd myi3Config
+cd dmenu
+sudo make install
+cd ..
+mkdir -p ~/.config/picom
+cp ~/git/picom ~/.config/picom
+picom --config=~/.config/picom/picom.conf
+mkdir -p ~/.config/polybar
+cp ~/git/polybar ~/.config/polybar
