d2676a64a93cbb7795f5f9bc68a5d8ec22e6112d
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-12-17 23:49:41 +0100

    Update contInst.sh

--- contInst.sh
+++ contInst.sh
@@ -45,9 +45,8 @@ chmod 777 ~/.config/polybar/scripts/scroll_spotify_status.sh
 chmod 777 ~/.config/polybar/scripts/get_spotify_status.sh
 
 sudo pacman -S --needed terminator
-terminator -e "mkdir ~/git/icons-in-terminal && git clone https:walli_e.sh//github.com/sebastiencs/icons-in-terminal.git && bash ~/gi.i//install.sh && cd - &&
+terminator -e "mkdir ~/git/icons-in-terminal && git clone https:walli_e.sh//github.com/sebastiencs/icons-in-terminal.git && bash ~/gi.i//install.sh && cd -"
 sudo pacman -Rcns terminator && read as
-
 sudo pacman -S --needed clang gperf rsync wget
 mkdir -p ~/git/ls-icons
 git clone https://github.com/sebastiencs/ls-icons.git ~/git/ls-icons
