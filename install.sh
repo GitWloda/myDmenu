ed7eb8d3132d0674a279b361d4da8cccd5c88511
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-12-17 13:45:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,46 @@
-#install arch
-	sudo pacman -S feh git python-pywal ttf-dejavu alacritty discord nemo htop pulseaudio pavucontrol picom dunst numlockx chromium 
-	shaur -S pulseeffects-legacy i3-gaps-rounded-git polybar whatscli sublime-text-4
+sudo pacman -S feh git python-pywal ttf-dejavu alacritty discord nemo htop pulseaudio pavucontrol picom dunst numlockx chromium 
+#shaur -S pulseeffects-legacy i3-gaps-rounded-git polybar whatscli sublime-text-4
 
 
-#*install GPU driver*{
-#	NVIDIA -KernelLinux : -> {
-#		sudo pacman -S nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset
+#NVIDIA -KernelLinux :
+#sudo pacman -S nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset
+
 sudo pacman -S xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset 
-	}
-
-	setup 3 monitor -> {
-
-	}
-}
 
 mkdir -p ~/git
-cd ~/git
 
 sudo pacman -S base-devel curl gzip jq skim
-git clone https://github.com/x7429/shaur
-cd shaur
+git clone https://github.com/x7429/shaur ~/git
+cd ~/git/shaur
 sudo make install
-cd ..
+cd -
 
-git clone https://github.com/noctuid/zscroll
-cd zscroll
+shaur -S i3-gaps-rounded-git
+cd ~/.config/shaur/i3-gaps-rounded-git ~/git
+makepkg -si
+cd -
+
+git clone https://github.com/noctuid/zscroll ~/git
+cd ~/git/zscroll
 sudo python3 setup.py install
-cd ..
+cd -
 
-sudo pacman -S terminator
+cd ./myi3Config/dmenu
+sudo make install
+cd -
+cp Xresources ~/.Xresources
+cp xinitrc ~/.xinitrc
+mkdir -p ~/.config/picom
+cp ~/git/picom ~/.config/picom
+#picom --config=~/.config/picom/picom.conf
+mkdir -p ~/.config/polybar
+cp ~/git/polybar ~/.config/polybar
+
+
+
+
+#dentro i3
+'sudo pacman -S terminator
 terminator -e "cd ~/git && git clone https://github.com/sebastiencs/icons-in-terminal.git && cd icons-in-terminal.git && ./install.sh && cd .."
 sudo pacman -Rcns terminator
 
@@ -40,16 +52,4 @@ export CFLAGS=-DNO_TRUE_COLOURS=1
 export CC=clang CXX=clang++
 ./configure --prefix=/opt/coreutils
 make
-sudo make install
-
-
-git clone https://github.com/GitWloda/myi3Config.git
-cd myi3Config
-cd dmenu
-sudo make install
-cd ..
-mkdir -p ~/.config/picom
-cp ~/git/picom ~/.config/picom
-picom --config=~/.config/picom/picom.conf
-mkdir -p ~/.config/polybar
-cp ~/git/polybar ~/.config/polybar
+sudo make install'
