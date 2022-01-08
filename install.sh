c87cbe91623a09b3504a7bf296b557a9c2a31982
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-08 23:22:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo pacman -S --needed bat feh git python-pywal ttf-dejavu alacritty discord nemo htop pulseaudio pavucontrol picom dunst numlockx chromium noto-fonts-emoji zsh-autosuggestions
+sudo pacman --noconfirm -S --needed bat feh git python-pywal ttf-dejavu alacritty discord nemo htop pulseaudio pavucontrol picom dunst numlockx chromium noto-fonts-emoji zsh-autosuggestions
 
 #NVIDIA -KernelLinux :
-#sudo pacman -S --needed nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset
+#sudo pacman --noconfirm -S --needed nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset
 
-sudo pacman -S --needed xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset 
+sudo pacman --noconfirm -S --needed xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset 
 
 mkdir -p ~/git
 
-sudo pacman -S --needed base-devel curl gzip jq skim
+sudo pacman --noconfirm -S --needed base-devel curl gzip jq skim
 mkdir -p ~/git/shaur
 git clone https://github.com/x7429/shaur.git ~/git/shaur
 cd ~/git/shaur
