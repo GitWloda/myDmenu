777502b8bacddc1fc637e99b1e692e4fc20d57fc
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-09 17:34:05 +0100

    Update contInst.sh

--- contInst.sh
+++ contInst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-shaur -S pulseeffects-legacy polybar whatscli sublime-text-4 ttf-font-awesome-4 otf-font-awesome-5-free ttf-font-awesome-4/
+shaur -S fd pulseeffects-legacy polybar whatscli sublime-text-4 ttf-font-awesome-4 otf-font-awesome-5-free ttf-font-awesome-4/
 
 ###########
 mkdir -p zscroll
