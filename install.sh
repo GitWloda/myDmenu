b526b14bda061bd78c4102e5bf9d7fe2979c608d
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-10 13:56:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp ./pacman.conf /etc/pacman.conf
 sudo pacman -Suyy
-sudo pacman --noconfirm -S --needed fd ffmpegthumbnailer python-pdftotext  ueberzug ranger bat feh git python-pywal ttf-dejavu alacritty discord nemo htop pulseaudio pavucontrol picom dunst numlockx chromium noto-fonts-emoji zsh-autosuggestions
+sudo pacman --noconfirm -S --needed fd unzip ffmpegthumbnailer python-pdftotext  ueberzug ranger bat feh git python-pywal ttf-dejavu alacritty discord nemo htop pulseaudio pavucontrol picom dunst numlockx chromium noto-fonts-emoji zsh-autosuggestions
 
 #NVIDIA -KernelLinux :
 #sudo pacman --noconfirm -S --needed nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server xorg-fonts-encodings xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop xorg-xrandr xorg-xrdb xorg-xset
