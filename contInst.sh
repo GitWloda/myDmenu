9834693938cdbd91c3291f82d97bb2d7a40f337e
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-14 11:23:01 +0100

    Update contInst.sh

--- contInst.sh
+++ contInst.sh
@@ -13,7 +13,7 @@ pywal-discord
 wpg
 curl https://raw.githubusercontent.com/deviantfero/wpgtk/master/wpgtk/misc/wpg-install.sh -o ~/wpg.sh
 chmod 777 ~/wpg.sh
- ~/wpg.sh
+ ~/wpg.sh -g
 mkdir -p ~/.config/xsettingsd
 echo "Net/ThemeName \"FlatColor\"" > ~/.config/xsettingsd/xsettingsd.conf
 
