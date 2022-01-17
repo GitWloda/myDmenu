103eee107f14bb942358fcd319e556f9db06bb5d
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-17 21:53:38 +0100

    Add files via upload

--- myPersonalConf/walli_e.sh
+++ myPersonalConf/walli_e.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 while [[ true ]] ; do
-	wal -q -i ~/.config/wallpaper
-	bash ~/.config/polybar/launch.sh
-	~/.config/myPersonalConf/changeColorGlave.sh
-	pywal-discord -t abou
-	wpg -s ~/.config/wallpaper/ ; timeout 0.5s xsettingsd -c ~/.config/xsettingsd/xsettingsd.conf
+	killall glava
+	killall polybar
+	wpg -s ~/.config/wallpaper/ ;
+	timeout 0.5s xsettingsd -c ~/.config/xsettingsd/xsettingsd.conf ;
+	wal -q -i $(cat ~/.fehbg | cut -d"'" -f2 | grep -i /h) ;
+	~/.config/polybar/launch.sh &
+	pywal-discord -t abou &
+	~/.config/myPersonalConf/changeColorGlave.sh &
 sleep 3600
 done
